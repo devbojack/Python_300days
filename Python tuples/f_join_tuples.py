a657eaef2d04329602628bdc3009c46db81c0914
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-11 03:21:50 +0300

    join tuples

---
+++ Python tuples/f_join_tuples.py
@@ -0,0 +1,14 @@
+# ____________________________________________________________________Python - Join Tuples
+# To join two or more tuples you can use the + operator:
+tuple1 = ('a', 'b', 'c')
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# ____________________________________________________________________Python - Multiply tuples
+# If you want to multiply the content of a tuple a given number of times, you can use the * operator:
+
+print("____________________________________________________________________Python - Multiply tuples")
+fruits = ("apple", "mango", "banana")
+multuple = fruits * 2
+print(multuple)
