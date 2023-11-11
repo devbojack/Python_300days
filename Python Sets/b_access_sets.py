c4de0045446013180f985bbcbfd724159b39906e
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-11 04:01:48 +0300

    access sets

---
+++ Python Sets/b_access_sets.py
@@ -0,0 +1,15 @@
+# ______________________________________________________________________ Python - Access Set Items
+# You cannot access items in a set by referring to an index or a key.
+# But you can loop through the set items using a for loop, or ask if a 
+        # specified value is present in a set, by using the in keyword.
+
+print("____________________________________________________ Python - Access Set Items")
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+# ______________________________________________________________________Check if is present
+print("____________________________________________________ Check if is present")
+thisset = {"apple", "banana", "cherry"}
+print("apple" in thisset)
+
